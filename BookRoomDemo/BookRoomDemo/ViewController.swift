fac0aa302e016ef85480a4c092d68bcd64e24242
XcqRomance <xcqromance@163.com>
2016-12-26 18:45:30 +0800

    demo

---
+++ BookRoomDemo/BookRoomDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BookRoomDemo
+//
+//  Created by romance on 2016/11/10.
+//  Copyright © 2016年 Romance. All rights reserved.
+//
+
+import UIKit
+import BookRoomKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+//    BookRoomManager.manager.test()
+  }
+  @IBAction func shwoDetail() {
+    
+    BookRoomManager.manager.showBookHomeVC(viewController: self)
+  }
+}
+
